2f142d45b4781e81e0307b8abfbc31ef944e0cd3
Hiroshac <hiroshachandramal01@gmail.com>
2021-11-13 18:32:53 +0530

    fixed

--- src/Components/headerbar.js
+++ src/Components/headerbar.js
@@ -2,21 +2,10 @@ import React from "react";
 
 const Headerbar = () =>{
     return(
-        <Nav>
-        <Bars />
-  
-        <NavMenu>
-        <NavLink to='/about' activeStyle>
-            Home
-          </NavLink>
-          <NavLink to='/about' activeStyle>
-            About
-          </NavLink>
-        </NavMenu>
-        <NavBtn>
-          <NavBtnLink to='/signin'>Sign In</NavBtnLink>
-        </NavBtn>
-      </Nav>
+  <div>
+
+    
+  </div>
 
     )
 }
--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Headeerbar />
+    <Headerbar />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
