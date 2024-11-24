56cde2c89d5617ee7906a0d98dac568112ecd0d0
Hiroshac <hiroshachandramal01@gmail.com>
2024-11-24 14:04:50 +0530

    development verssion 2 in hirosha web

---
+++ src/Components/home/CTA.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function CTA() {
+  return (
+    <div className='cta'>
+        {/* <a href='https://drive.google.com/file/d/1bp3oCTlLvQk23j5KIZap5cXdE1jMdkka/view?usp=sharing'>Download CV</a> */}
+        <a href='https://drive.google.com/uc?export=1bp3oCTlLvQk23j5KIZap5cXdE1jMdkka'>Download CV</a>
+        <a href='#contact'>Let's Talk</a>
+    </div>
+  )
+}
+
+export default CTA
--- src/Components/home/Home.js
+++ src/Components/home/Home.js
@@ -1,25 +1,30 @@
 import React from "react";
+import HiImage from "../../image/Hi.jpg";
+import Hi2Image from "../../image/H-1.png";
+import CTA from "./CTA";
+import "./home.css";
+
 
 
 export default function Home() {
   return (
-    <center>
-     <section id ='#' >
-            <h1>Hello, I am </h1>
-      
-        <img src={require('../../image/Hi.jpg').default} style={{width:"300px", height:"400px",}}/>
-     
-      <h1> HIROSHA CHANDRAMAL </h1>
-      <h4>I am 22-year-old Undergraduate Software Engineer in Sri Lanka Institute of Information Technology.</h4>
-      <h4>I am Fullstack Developer.</h4>
-      <div>
-      </div>
-    </section>
-    </center>
-    
+    // <center>
+    <di className="container header__container">
+      <section id="#">
+        <h1>Hello, I am </h1>
 
-   
+        <img src={Hi2Image} style={{ width: "300px", height: "400px" }} />
 
-
+        <h1> HIROSHA CHANDRAMAL </h1>
+        <h4>
+          I am 22-year-old Undergraduate Software Engineer in Sri Lanka
+          Institute of Information Technology.
+        </h4>
+        <h4>I am Fullstack Developer.</h4>
+        <CTA />
+        <div></div>
+      </section>
+    </di>
+    // </center>
   );
 }
